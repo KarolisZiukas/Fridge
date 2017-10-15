6f53cdf94cead6680559444095882f45230d80c9
Karolis <karolis.ziu@gmail.com>
2017-10-15 13:03:39 +0300

    recycler view and develop branch

---
+++ app/src/main/java/com/example/karolis/whatsinmyfridge/TempModel.java
@@ -0,0 +1,24 @@
+package com.example.karolis.whatsinmyfridge;
+
+
+public class TempModel {
+
+    private String name;
+    private String data;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
